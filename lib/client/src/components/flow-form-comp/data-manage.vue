513bdc3f9a5aa6ac7232d01cd9e6b75b7f653c81
terlinhe <terlinhe@tencent.com>
2022-06-20 20:05:55 +0800

    feat: 生成nocode类型页面源码

---
+++ lib/client/src/components/flow-form-comp/data-manage.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        {{ type }}
+        {{ config }}
+        {{ formIds }}
+        {{ serviceId }}
+    </section>
+
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                required: 'true'
+            },
+            config: {
+                type: Object,
+                default: () => ({})
+            },
+            formIds: {
+                type: [Object, Number],
+                required: true
+            },
+            serviceId: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
