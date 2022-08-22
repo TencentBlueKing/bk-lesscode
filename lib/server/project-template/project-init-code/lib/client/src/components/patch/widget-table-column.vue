b4cf73d62ae6fe9d4bf1037e004d7c5a9318bca2
xuzhan <xuzhan@tencent.com>
2022-08-22 22:08:17 +0800

    修复表格展示异常

--- lib/server/project-template/project-init-code/lib/client/src/components/patch/widget-table-column.vue
+++ lib/server/project-template/project-init-code/lib/client/src/components/patch/widget-table-column.vue
@@ -1,12 +1,9 @@
 <template>
     <component
         :is="type"
-        :label="item.label"
-        :prop="item.prop"
-        :sortable="item.sortable"
-        :type="item.type"
-        :width="item.width"
         :formatter="bkTableFormatter"
+        :render-header="renderHeader"
+        v-bind="item"
     />
 </template>
 
@@ -14,20 +11,97 @@
     import dayjs from 'dayjs'
 
     export default {
+        name: 'widget-table-column',
+
         props: {
             type: String,
             item: Object
         },
 
+        data () {
+            return {
+                filterText: ''
+            }
+        },
+
         methods: {
+            renderHeader (h, { column, $index }) {
+                const vm = this
+                return h(
+                    'div',
+                    {},
+                    [
+                        column.label,
+                        this.item.filterable
+                            ? h(
+                                'bk-popover',
+                                {
+                                    props: {
+                                        trigger: 'click',
+                                        theme: 'light',
+                                        extCls: 'g-popover-empty-padding'
+                                    },
+                                    ref: 'popoverRef',
+                                    refInFor: true
+                                },
+                                [
+                                    h(
+                                        'bk-input',
+                                        {
+                                            slot: 'content',
+                                            props: {
+                                                value: vm.filterText,
+                                                placeholder: '请输入并按回车键进行搜索'
+                                            },
+                                            on: {
+                                                enter () {
+                                                    vm.handleFilter(column)
+                                                },
+                                                change (val) {
+                                                    vm.changeFilterText(val)
+                                                }
+                                            }
+                                        }
+                                    ),
+                                    h(
+                                        'i',
+                                        {
+                                            class: 'bk-table-column-filter-trigger bk-icon icon-funnel',
+                                            slot: 'default'
+                                        }
+                                    )
+                                ]
+                            )
+                            : ''
+                    ]
+                )
+            },
+
             bkTableFormatter (row, column, cellValue, index) {
                 if (typeof cellValue === 'object') {
-                    return JSON.stringify(cellValue)
-                } else if (['createTime', 'updateTime'].includes(column.property)) {
+                    return cellValue ? JSON.stringify(cellValue) : '--'
+                } else if (/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(cellValue)) {
                     return cellValue ? dayjs(cellValue).format('YYYY-MM-DD HH:mm:ss') : '--'
                 } else {
                     return cellValue
                 }
+            },
+
+            handleFilter (column) {
+                this.$parent.$refs.tableHeader.$refs.popoverRef.forEach(refItem => refItem.hideHandler())
+                this.$parent.$parent.handleFilter({
+                    key: column.property,
+                    value: this.filterText
+                })
+            },
+
+            changeFilterText (val) {
+                this.filterText = val
+            },
+
+            filterMethod (value, row, column) {
+                const property = column.property
+                return row[property] === value
             }
         }
     }
