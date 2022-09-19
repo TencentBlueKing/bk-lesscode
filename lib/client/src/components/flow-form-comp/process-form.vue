10e1e9dd83b721560c70aabc1de528fe92171063
luofann <fannluowork@gmail.com>
2022-09-19 11:34:56 +0800

    minor: 错误处理未匹配到目标文本时，返回原始值

--- lib/client/src/components/flow-form-comp/process-form.vue
+++ lib/client/src/components/flow-form-comp/process-form.vue
@@ -172,6 +172,7 @@
                                 if (typeof message === 'string' && message.includes('服务未启用')) {
                                     return '当前流程未部署，请部署后提单'
                                 }
+                                return message
                             }
                         }
                         const path = this.versionId ? '/nocode/ticket/create_ticket_with_version/' : '/nocode/v2/itsm/create_ticket/'
