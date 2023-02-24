b969c47a31ccc4291c1b0f031859684aa220b9f8
panyuji <panyuji@canway.net>
2023-02-24 16:14:45 +0800

    [将新增评分组件的相关代码同步到项目模板中]

---
+++ lib/server/project-template/project-init-code/lib/client/src/components/flow-form-comp/form/fields/rate.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <bk-rate :rate.sync="defaultValue" :edit="!disabled" @score="chooseRate"></bk-rate>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Rate',
+        props: {
+            value: {
+                type: [Number, String],
+                default: '{}'
+            },
+            disabled: {
+                type: Boolean,
+                default: true
+            },
+            field: {
+                type: Object,
+                default: () => ({})
+            }
+        },
+        data () {
+            return {
+                defaultValue: parseInt(this.value || 0)
+            }
+        },
+        computed: {},
+        watch: {
+            value (newValue, oldValue) {
+                this.defaultValue = parseInt(newValue || 0)
+            }
+        },
+        mounted () {
+        
+        },
+
+        methods: {
+            chooseRate (val) {
+                this.$emit('change', val)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
