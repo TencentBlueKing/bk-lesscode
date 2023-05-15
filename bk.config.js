037fa82707b33a42851c664cd11a457641ec5419
xuzhan <xuzhan@tencent.com>
2023-05-15 15:21:58 +0800

    支持vue3国际化

--- bk.config.js
+++ bk.config.js
@@ -41,7 +41,8 @@ module.exports = {
             resolve: {
                 alias: {
                     '@': path.resolve(__dirname, './lib/client/src'),
-                    'shared': path.resolve(__dirname, './lib/shared')
+                    'shared': path.resolve(__dirname, './lib/shared'),
+                    VueI18n: path.resolve(__dirname, './node_modules/vue-i18n'),
                 },
                 fallback: {
                     buffer: require.resolve('buffer')
