28d97d6e0e006e08442fedb62fde41965dec0fa2
xuzhan <xuzhan@tencent.com>
2023-06-29 22:21:15 +0800

    权限优化

--- lib/server/project-template/vue3/project-init-code/lib/client/src/components/apply-permission/ask-permission.vue
+++ lib/server/project-template/vue3/project-init-code/lib/client/src/components/apply-permission/ask-permission.vue
@@ -9,11 +9,11 @@
                 <bk-table-column
                     :width="300"
                     :label="actionText">
-                    <template slot-scope="{ row }">{{ row.actionName }}</template>
+                    <template #default="{ row }">{{ row.actionName }}</template>
                 </bk-table-column>
                 <bk-table-column
                     :label="resourceText">
-                    <template slot-scope="{ row }">
+                    <template #default="{ row }">
                         <div class="resource-content">
                             <template v-if="row.relatedResources.length > 0">
                                 <p v-for="(resource, index) in row.relatedResources" :key="index">
@@ -25,9 +25,9 @@
                         </div>
                     </template>
                 </bk-table-column>
-                <div slot="empty">
+                <template #empty>
                     <span v-if="!loading">{{ errorTips }}</span>
-                </div>
+                </template>
             </bk-table>
         </div>
     </div>
