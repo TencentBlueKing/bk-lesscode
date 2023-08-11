4ef58f921c13a55afeccd195f55cd6f69494b46c
miaomiao <13407923955@163.com>
2023-08-11 10:48:18 +0800

    fix:画布中函数管理中的label字体为12px

--- lib/client/src/views/index/components/material-panel/components/panel-icon/index.vue
+++ lib/client/src/views/index/components/material-panel/components/panel-icon/index.vue
@@ -7,7 +7,7 @@
             :change-tab="handleChangeTab"
         >
         </select-tab>
-        <div class="drag-component-list">
+        <div :class="isExternal ? 'only-official-icon' : 'drag-component-list' ">
             <component :is="com" :icon-type="tab" />
         </div>
     </div>
@@ -64,14 +64,17 @@
 <style lang="postcss" scoped>
     @import "@/css/mixins/ellipsis";
 
-    .panel-component{
+    .panel-component {
         height: 100%;
-        .search-box{
+        .search-box {
             padding: 6px 12px;
         }
-        .drag-component-list{
+        .drag-component-list {
             height: calc(100% - 42px);
             padding-bottom: 10px;
+        }
+        .only-official-icon {
+            height: 100%;
         }
     }
 </style>
