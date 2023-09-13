e4db141601a2a94fa52bf8bc409f42663844609b
miaomiao <13407923955@163.com>
2023-09-13 18:08:05 +0800

    fix: AI小助手体验样式修复

--- lib/client/src/views/index/components/material-panel/components/panel-ai/components/content.vue
+++ lib/client/src/views/index/components/material-panel/components/panel-ai/components/content.vue
@@ -18,6 +18,8 @@
         >
             <bk-input
                 size="large"
+                ref="inputRef"
+                :native-attributes="{ autofocus: 'autofocus' }"
                 class="ai-operation-input"
                 :disabled="isLoading"
                 v-model="content"
@@ -40,7 +42,8 @@
     import {
         ref,
         onBeforeMount,
-        nextTick
+        nextTick,
+        watch
     } from '@vue/composition-api'
     import LC from '@/element-materials/core'
     import {
@@ -57,19 +60,21 @@
     import store from '@/store'
     import vue2Materials from '@/element-materials/materials/vue2'
     import vue3Materials from '@/element-materials/materials/vue3'
+    import { bkMessage } from 'bk-magic-vue'
 
     export default {
         components: {
             RenderMessage
         },
-
-        setup () {
+        props: { isShowAi: Boolean },
+        setup (props) {
             let currentMessage = ref({})
             let cmdMessage = ''
             let errorTime = 0
             const messages = ref([])
             const content = ref('')
             const isLoading = ref(false)
+            const inputRef = ref(null)
 
             const scrollToBottom = () => {
                 nextTick(() => {
@@ -168,6 +173,7 @@
                 messages.value = []
                 isLoading.value = false
                 aiHelper.clearPrompt()
+                bkMessage({ theme: 'success', message: window.i18n.t('聊天记录清空成功'), limit: 1 })
                 pushMessage('ai', 'Hi，我是小鲸，我可以帮你实现添加组件，函数生成等复杂功能，你可以尝试说帮我新增一个表格')
             }
 
@@ -431,11 +437,20 @@
             onBeforeMount(() => {
                 pushMessage('ai', 'Hi，我是小鲸，我可以帮你实现添加组件，函数生成等复杂功能，你可以尝试说帮我新增一个表格')
             })
-
+            watch(
+                () => props.isShowAi,
+                () => {
+                    if (props.isShowAi) {
+                        const $autoFocusItem = inputRef.value.$el.querySelector('[autofocus="autofocus"]')
+                        $autoFocusItem && $autoFocusItem.focus()
+                    }
+                }
+            )
             return {
                 messages,
                 content,
                 isLoading,
+                inputRef,
                 handleUserInput,
                 clearMessage
             }
@@ -476,5 +491,6 @@
     .ai-operation-button {
         padding: 0 3px 0 0 !important;
         min-width: 84px;
+        font-size: 14px;
     }
 </style>
