0076bf0f70eb9a2fdbadcf0477bb2589f2701e47
terlinhe <terlinhe@tencent.com>
2024-01-18 10:24:50 +0800

    fix: 交互样式优化 # Reviewed, transaction id: 1996

--- lib/client/src/views/project/backend-module/children/backend/components/render-nodes.vue
+++ lib/client/src/views/project/backend-module/children/backend/components/render-nodes.vue
@@ -1,5 +1,5 @@
 <template>
-    <section v-bkloading="{ isLoading }" style="width: 100%; height: 100%;">
+    <section v-bkloading="{ isLoading }" class="render-node-wrapper" >
         <empty-status v-if="!storyList.length" type="custom" :part="false">
             <div>
                 <span>{{$t('请先添加需求描述')}}</span>
@@ -188,6 +188,8 @@
                         if (other.id === 'PreviewProcessor') {
                             other.url = node?.content?.result?.app_url
                         }
+                        other.property = node.property
+                        other.content = node.content
                     }
                     
                 })
@@ -474,79 +476,84 @@
 </script>
 
 <style lang="postcss" scoped>
-.node {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    border: 1px solid #c2c8d5;
-    border-left: 4px solid #5F95FF;
-    border-radius: 4px;
-    box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.06);
-}
-.node img {
-    width: 20px;
-    height: 20px;
-    flex-shrink: 0;
-    margin-left: 8px;
-}
-.node .label {
-    display: inline-block;
-    flex-shrink: 0;
-    width: 104px;
-    margin-left: 8px;
-    color: #666;
-    font-size: 12px;
-}
-.node .status {
-    flex-shrink: 0;
-}
-.node.success {
-    border-left: 4px solid #52c41a;
-}
-.node.failed {
-    border-left: 4px solid #ff4d4f;
-}
-.node.running .status img {
-    animation: spin 1s linear infinite;
-}
-.x6-node-selected .node {
-    border-color: #1890ff;
-    border-radius: 2px;
-    box-shadow: 0 0 0 4px #d4e8fe;
-}
-.x6-node-selected .node.success {
-    border-color: #52c41a;
-    border-radius: 2px;
-    box-shadow: 0 0 0 4px #ccecc0;
-}
-.x6-node-selected .node.failed {
-    border-color: #ff4d4f;
-    border-radius: 2px;
-    box-shadow: 0 0 0 4px #fedcdc;
-}
-.x6-edge:hover path:nth-child(2){
-    stroke: #1890ff;
-    stroke-width: 1px;
-}
+    .render-nodes-wrapper {
+        width: 100%;
+        height: calc(100% - 148px);
+        .node {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            background-color: #fff;
+            border: 1px solid #c2c8d5;
+            border-left: 4px solid #5F95FF;
+            border-radius: 4px;
+            box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.06);
+        }
+        .node img {
+            width: 20px;
+            height: 20px;
+            flex-shrink: 0;
+            margin-left: 8px;
+        }
+        .node .label {
+            display: inline-block;
+            flex-shrink: 0;
+            width: 104px;
+            margin-left: 8px;
+            color: #666;
+            font-size: 12px;
+        }
+        .node .status {
+            flex-shrink: 0;
+        }
+        .node.success {
+            border-left: 4px solid #52c41a;
+        }
+        .node.failed {
+            border-left: 4px solid #ff4d4f;
+        }
+        .node.running .status img {
+            animation: spin 1s linear infinite;
+        }
+        .x6-node-selected .node {
+            border-color: #1890ff;
+            border-radius: 2px;
+            box-shadow: 0 0 0 4px #d4e8fe;
+        }
+        .x6-node-selected .node.success {
+            border-color: #52c41a;
+            border-radius: 2px;
+            box-shadow: 0 0 0 4px #ccecc0;
+        }
+        .x6-node-selected .node.failed {
+            border-color: #ff4d4f;
+            border-radius: 2px;
+            box-shadow: 0 0 0 4px #fedcdc;
+        }
+        .x6-edge:hover path:nth-child(2){
+            stroke: #1890ff;
+            stroke-width: 1px;
+        }
 
-.x6-edge-selected path:nth-child(2){
-    stroke: #1890ff;
-    stroke-width: 1.5px !important;
-}
+        .x6-edge-selected path:nth-child(2){
+            stroke: #1890ff;
+            stroke-width: 1.5px !important;
+        }
 
-@keyframes running-line {
-    to {
-        stroke-dashoffset: -1000;
+        @keyframes running-line {
+            to {
+                stroke-dashoffset: -1000;
+            }
+        }
+        @keyframes spin {
+            from {
+                transform: rotate(0deg);
+            }
+            to {
+                transform: rotate(360deg);
+            }
+        }
     }
-}
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-}
+
 </style>
